20b119d20e8bb0114f0dbd8563614a9fc97e54e4
jason <jason@jbrmmg.me.uk>
2020-08-20 09:34:22 +0100

    Update based on lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jbr-root',
-  template: `
-    <nav class='navbar navbar-expand navbar-light bg-light'>
-      <img src="assets/images/logo.svg" height="30" width="30" />
-      <a class='navbar-brand'>{{pageTitle}}</a>
-      <ul class='nav nav-pills'>
-        <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
-        <li><a class='nav-link' [routerLink]="['/logs']">Logs</a></li>
-        <li><a class='nav-link' [routerLink]="['/podcast']">Podcast</a></li>
-        <li><a class='nav-link' [routerLink]="['/money']">Money</a></li>
-        <li><a class='nav-link' [routerLink]="['/bup']">Backups</a></li>
-        <li><a class='nav-link' [routerLink]="['/house']">House</a></li>
-        <li><a class='nav-link' [routerLink]="['/weight']">Weight</a></li>
-      </ul>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent {
--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -9,13 +9,13 @@ export class WelcomeComponent {
   textData1: string;
   textData2: string;
 
-  constructor(private _welcomeService : WelcomeService) {
+  constructor(private _welcomeService: WelcomeService) {
 
   }
 
   onClick() {
     // Send the data.
-    this._welcomeService.SendData(this.textData1,this.textData2);
+    this._welcomeService.SendData(this.textData1, this.textData2);
 
     // Reset
     this.textData1 = '';
