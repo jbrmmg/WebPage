f10b914324e212ea6993eb4e4123c185fe2edf72
jason <jason@jbrmmg.me.uk>
2022-01-02 18:45:43 +0000

    Upgrade the UI

--- src/app/money/calculator/money-calc-btn-row.ts
+++ src/app/money/calculator/money-calc-btn-row.ts
@@ -3,9 +3,6 @@ import {MoneyCalcButton} from './money-calc-btn';
 export class MoneyCalcBtnRow {
     columns: Array<MoneyCalcButton> = [];
 
-    constructor() {
-    }
-
     public addColumn(column: MoneyCalcButton) {
         this.columns.push(column);
     }
