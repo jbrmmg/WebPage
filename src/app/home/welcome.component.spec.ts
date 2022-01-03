0043d687ecbdf1cdda9efac2f7118ff302e37a0e
jason <jason@jbrmmg.me.uk>
2022-01-03 09:06:37 +0000

    Tidy up

---
+++ src/app/home/welcome.component.spec.ts
@@ -0,0 +1,50 @@
+import { WelcomeService } from './welcome.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { Type } from '@angular/core';
+import { WelcomeComponent } from './welcome.component';
+
+describe('WelcomeService', () => {
+    let fixture: ComponentFixture<WelcomeComponent>;
+    let app: WelcomeComponent;
+    let httpMock: HttpTestingController;
+
+    describe('WelcomeService', () => {
+        beforeEach(async () => {
+            TestBed.configureTestingModule({
+                imports: [
+                    HttpClientTestingModule,
+                    FormsModule,
+                ],
+                declarations: [
+                    WelcomeComponent,
+                ],
+                providers: [
+                    WelcomeService,
+                ]
+            });
+
+            await TestBed.compileComponents();
+
+            fixture = TestBed.createComponent(WelcomeComponent);
+            app = fixture.componentInstance;
+            httpMock = fixture.debugElement.injector.get<HttpTestingController>(HttpTestingController as Type<HttpTestingController>);
+
+            fixture.detectChanges();
+        });
+
+        afterEach(() => {
+            httpMock.verify();
+        });
+
+        it('test your http call', () => {
+            app.textData1 = 'x';
+            app.textData2 = 'y';
+            app.onClick();
+
+            const req = httpMock.expectOne('/podcast/reminder');
+            expect(req.request.method).toBe('POST');
+        });
+    });
+});
