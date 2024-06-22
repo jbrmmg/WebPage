662f49b8b00d590a37d24a5044f5e999a27e4a7c
jason <jason@jbrmmg.me.uk>
2024-06-22 19:37:11 +0100

    JBR-421 - new UI components

--- src/app/money/grid/header/grid-header-account.ts
+++ src/app/money/grid/header/grid-header-account.ts
@@ -1,15 +1,71 @@
-import {Component, Input} from "@angular/core";
-import {FilterAccount} from "../filter/filter-account";
+import {Component, Input, OnInit, TemplateRef} from "@angular/core";
+import {ButtonsModule} from "ngx-bootstrap/buttons";
+import {NgForOf, NgIf} from "@angular/common";
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {MoneyService} from "../../money.service";
+
+class AccountFilterOption {
+    id: string;
+    display: string;
+    all: boolean;
+}
 
 @Component({
     selector: 'jbr-grid-header-account',
     templateUrl: './grid-header-account.html',
     styleUrls: ['./grid-header-account.css'],
     imports: [
-        FilterAccount
+        ButtonsModule,
+        NgForOf,
+        NgIf
     ],
     standalone: true
 })
-export class GridHeaderAccount {
+export class GridHeaderAccount implements OnInit{
     @Input() header: string;
+    filter: string;
+    modalRef: BsModalRef;
+    accounts: AccountFilterOption[] = [];
+    errorMessage: string;
+
+    constructor(private modalService: BsModalService,
+                private _moneyService: MoneyService ) {
+        let all: AccountFilterOption = new AccountFilterOption();
+        all.id = "";
+        all.display = "All"
+        all.all = true;
+
+        this.accounts.push(all)
+        this.filter = "(all)";
+    }
+
+    ngOnInit(): void {
+        this._moneyService.getAccounts().subscribe({
+            next: (accounts) => {
+                accounts.forEach(value => {
+                    let next: AccountFilterOption = new AccountFilterOption();
+                    next.all = false;
+                    next.display = value.name;
+                    if(value.closed) {
+                        next.display += " (c)";
+                    }
+                    next.id = value.id;
+
+                    this.accounts.push(next);
+                })
+            },
+            error: (response) => this.errorMessage = <any> response,
+            complete: () => {
+                console.log("Account Options Loaded")
+            }
+        });
+    }
+
+    getAccountImage(id: string) : string {
+        return MoneyService.getAccountImage(id);
+    }
+
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template, {});
+    }
 }
