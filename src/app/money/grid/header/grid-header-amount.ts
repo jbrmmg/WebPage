f52768fd282425cfd0a9a4918c813e4e936692c2
jason <jason@jbrmmg.me.uk>
2024-07-23 18:24:03 +0100

    JBR-421 screen for value range.

--- src/app/money/grid/header/grid-header-amount.ts
+++ src/app/money/grid/header/grid-header-amount.ts
@@ -1,18 +1,38 @@
 import {Component, TemplateRef} from "@angular/core";
-import {GridHeader} from "./grid-header";
+import {FilterEvent, GridHeader} from "./grid-header";
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {BsDatepickerModule} from "ngx-bootstrap/datepicker";
+import {DatePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {ValueRange} from "../../range/valueRange";
+import {HeaderType} from "./grid-header-type";
 
 @Component({
     selector: 'jbr-grid-header-amount',
     templateUrl: './grid-header-amount.html',
     styleUrls: ['./grid-header-amount.css'],
+    imports: [
+        BsDatepickerModule,
+        DatePipe,
+        FormsModule
+    ],
     standalone: true
 })
 export class GridHeaderAmount extends GridHeader {
     modalRef: BsModalRef;
+    fromAmount: string;
+    toAmount: string;
 
     constructor(private modalService: BsModalService ) {
         super();
+    }
+
+    fromChanged(event: any) {
+        this.fromAmount = event.target.value;
+    }
+
+    toChanged(event: any) {
+        this.toAmount = event.target.value;
     }
 
     exit() {
@@ -20,6 +40,46 @@ export class GridHeaderAmount extends GridHeader {
     }
 
     openModal(template: TemplateRef<any>) {
+        if(this.filter != null && this.filter.valueRange != null) {
+            this.fromAmount = this.filter.valueRange.minimum.toString();
+            this.toAmount = this.filter.valueRange.maximum.toString();
+        }
+
         this.modalRef = this.modalService.show(template, {class: 'modal-lg'});
+    }
+
+    clear() {
+        this.modalRef.hide();
+
+        if(this.filter != null) {
+            this.filter.valueRange = null;
+        }
+
+        let event: FilterEvent = new FilterEvent();
+        event.source = HeaderType.Credit;
+        this.filterChanged.emit(event);
+    }
+
+    selectAmounts() {
+        this.modalRef.hide();
+
+        if(this.filter != null) {
+            let min: number = parseInt(this.fromAmount);
+            let max: number = parseInt(this.toAmount);
+
+            if(min > max) {
+                this.fromAmount = max.toString();
+                this.toAmount = min.toString();
+
+                min = parseInt(this.fromAmount);
+                max = parseInt(this.toAmount);
+            }
+
+            this.filter.valueRange = new ValueRange(min,max);
+
+            let event: FilterEvent = new FilterEvent();
+            event.source = HeaderType.Credit;
+            this.filterChanged.emit(event);
+        }
     }
 }
