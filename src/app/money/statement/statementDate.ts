4761cf076c947ac680d5f7339e7b89d60526581e
jason <jason@jbrmmg.me.uk>
2024-09-06 17:59:12 +0100

    JBR-407 add further tests.

--- src/app/money/statement/statementDate.ts
+++ src/app/money/statement/statementDate.ts
@@ -3,7 +3,10 @@
 */
 
 export class StatementDate {
+    none: boolean;
+
     constructor(public year: number,
                 public month: number) {
+        this.none = year == null && month == null;
     }
 }
